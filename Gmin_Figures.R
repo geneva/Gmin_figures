830a7d8b7064be72728b817e43c9f4f01cc5e621
geneva <geneva@crescatscientia.com>
2014-05-09 16:06:02 -0400

    Plotting works! optomized for 5 chromsomes right now

--- Gmin_Figures.R
+++ Gmin_Figures.R
@@ -1,22 +1,17 @@
 load("/Users/ageneva/Dropbox/code_snippets/git/gmin_figures/gmin.RData")
 
-plot.gmin <- function(x,cutoff){
+plot.gmin <- function(x,cutoff,nm){
   x <- x[which(x$nsites>cutoff),]
   gmin.z <- scale(x$gmin)
   gmin.p <- pnorm(gmin.z)
   x<- cbind(x,gmin.p)
-  par(oma=c(8,4,8,4), mar=c(0.5,6,0.5,1))
+  par(mar=c(3,5,2,2))
   plot(data=x, gmin~start, ylim=c(0,1), type="n", pch=20, las=1, xlab="", ylab="", xaxt="n",axes=F)
   box(lwd=2)
-  mtext(expression(italic("G")[min]), side=2, line=4, cex=1.7)
+  #mtext(expression(italic("G")[min]), side=2, line=4, cex=1.7)
+  mtext(nm, side=2, cex=1.7, line=3)
   axis(2, las=1, cex.axis=1.5)
   axis(1, at=c(0,5,10,15,20), labels=c(0,5,10,15,20), cex.axis=1.5)
-  rect(2503253,-0.03,3063232,1.03, col="gray88", border=NA)
-  rect(4917507,-0.03,6037228,1.03, col="gray88", border=NA)
-  rect(7082078,-0.03,8046464,1.03, col="gray88", border=NA)
-  rect(9666210,-0.03,11272604,1.03, col="gray88", border=NA)
-  rect(13823804,-0.03,15335968,1.03, col="gray88", border=NA)
-  rect(15647600,-0.03,22422826,1.03, col="gray88", border=NA)
   #abline(h=0.28, lty=3) set this to zscore cutoff
   points(data=x, gmin~start, pch=20)
   lines(data=x, gmin~start)
@@ -26,9 +21,10 @@ plot.gmin <- function(x,cutoff){
 
 
 
+layout(matrix(c(1,2,3,4,5),5,1,byrow=TRUE))
+plot.gmin(chr2L,5000, "2L")
+plot.gmin(chr2R,5000, "2R")
+plot.gmin(chr3L,5000, "3L")
+plot.gmin(chr3R,5000, "3R")
+plot.gmin(chrX,5000, "X")
 
-plot.gmin(chr2L,1000)
-plot.gmin(chr2R,1000)
-plot.gmin(chr3L,1000)
-plot.gmin(chr3R,1000)
-plot.gmin(chrX,1000)
