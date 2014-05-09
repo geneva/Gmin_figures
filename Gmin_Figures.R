16224e142a39babbe9bb03f26eabd009dfb94f42
geneva <geneva@crescatscientia.com>
2014-05-09 16:22:50 -0400

    README added

--- Gmin_Figures.R
+++ Gmin_Figures.R
@@ -7,16 +7,15 @@ plot.gmin <- function(x,cutoff,nm){
   x<- cbind(x,gmin.p)
   par(mar=c(3,5,2,2))
   plot(data=x, gmin~start, ylim=c(0,1), type="n", pch=20, las=1, xlab="", ylab="", xaxt="n",axes=F)
+  rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "skyblue")
   box(lwd=2)
-  #mtext(expression(italic("G")[min]), side=2, line=4, cex=1.7)
   mtext(nm, side=2, cex=1.7, line=3)
   axis(2, las=1, cex.axis=1.5)
   axis(1, at=c(0,5,10,15,20), labels=c(0,5,10,15,20), cex.axis=1.5)
-  #abline(h=0.28, lty=3) set this to zscore cutoff
   points(data=x, gmin~start, pch=20)
   lines(data=x, gmin~start)
-  points(data=x[which(x$gmin.p<0.05),],gmin~start,pch=21, col="black")
-  points(data=x[which(x$gmin.p<0.05),],gmin~start,pch=20, col="white")
+  points(data=x[which(x$gmin.p<0.01),],gmin~start,pch=21, col="black")
+  points(data=x[which(x$gmin.p<0.01),],gmin~start,pch=20, col="white")
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Gmin_figures
 ============
+
+R Functions and scripts to plot Gmin [arXiv link here] across chromosomes. Building for SMBE poster with modularity in mind...
